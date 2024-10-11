bf2ef14820b855ddc3a99a321bd7942fc48c2375
ugen <ugen@google.com>
2024-10-11 18:03:36 +0000

    Manually cherry picking changes from c1e58cdf2b123a2a6ca7c63370d286cdf544b271

--- src/main/java/liqp/filters/Strip_HTML.java
+++ src/main/java/liqp/filters/Strip_HTML.java
@@ -1,9 +1,21 @@
 package liqp.filters;
 
 import liqp.TemplateContext;
-import org.jsoup.Jsoup;
+
+import java.util.regex.Pattern;
 
 public class Strip_HTML extends Filter {
+
+    // STRIP_HTML_BLOCKS = Regexp.union(
+    //      /<script.*?<\/script>/m,
+    //      /<!--.*?-->/m,
+    //      /<style.*?<\/style>/m
+    //    )
+    private static final Pattern STRIP_HTML_BLOCKS = Pattern.compile("<script.*?</script>|<style.*?</style>|<!--.*?-->", Pattern.MULTILINE);
+
+    // STRIP_HTML_TAGS = /<.*?>/m
+    private static final Pattern STRIP_HTML_TAGS = Pattern.compile("<.*?>", Pattern.MULTILINE);
+
 
     /*
      * strip_html(input)
@@ -15,6 +27,9 @@ public class Strip_HTML extends Filter {
 
         String html = super.asString(value, context);
 
-        return Jsoup.parse(html).text();
+        html = STRIP_HTML_BLOCKS.matcher(html).replaceAll("");
+        html = STRIP_HTML_TAGS.matcher(html).replaceAll("");
+
+        return html;
     }
 }
